f22046f19681868023e62eb5ece7c8ab24ede6fd
Md Rafiqul Islam <rafiverse04@gmail.com>
2025-07-13 20:55:45 +0600

    Update problem_06_armstrong_number_checker.c

--- problem_06_armstrong_number_checker.c
+++ problem_06_armstrong_number_checker.c
@@ -1,5 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-//Write a program to check if a given number is Armstrong number or not.
+//Write a program to check if a given number is Armstrong number or not
 
-int main()
+int main() {
+    int num, originalNum, remainder, result = 0, n = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        originalNum /= 10;
+        n++;
+    }
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
